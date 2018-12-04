b7facad9e88c5d3f57625fc33c83338769f2176b
Mike Webster <mwebster@wyzant.com>
2018-12-03 20:21:53 -0600

    add config struct and load the file

---
+++ config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// Config represents all configuration values
+type Config struct {
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Keywords    []string `json:"keywords"`
+	Website     string   `json:"website"`
+	Repository  string   `json:"repository"`
+	Port        int      `json:"port"`
+	Slack       struct {
+		WebHook string `json:"webhook"`
+	}
+}
+
+var cfg *Config
+
+// GetConfig will load the configuration from the given path unless we already have a cached value. If it is loaded, it is then cached
+func GetConfig(file string) *Config {
+	if cfg != nil {
+		return cfg
+	}
+
+	var config Config
+	configFile, err := os.Open(file)
+	defer configFile.Close()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+
+	cfg = &config
+
+	return cfg
+}
