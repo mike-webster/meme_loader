4766f9d6a758c0af459113cdf09b3da9ee180b04
Mike Webster <mwebster@wyzant.com>
2018-12-03 20:35:40 -0600

    try send message from app

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/mike-webster/meme_loader/config"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	cfg := config.GetConfig("app.json")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	if cfg == nil {
+		log.Fatal("Cant parse config")
 	}
 
 	router := gin.New()
@@ -23,5 +27,35 @@ func main() {
 		c.JSON(http.StatusOK, "{'status':'ok'}")
 	})
 
-	router.Run(":" + port)
+	router.GET("/test", func(c *gin.Context) {
+		defer func() {
+			if r := recover(); r != nil {
+				// we panicked
+				log.Println("we panicked!\n", r)
+				c.JSON(http.StatusInternalServerError, "{'message':'unsent'}")
+			} else {
+				c.JSON(http.StatusOK, "{'message':'sent'}")
+			}
+		}()
+
+		sendToSlack(cfg)
+	})
+
+	router.Run(":" + fmt.Sprint(cfg.Port))
+}
+
+func sendToSlack(cfg *config.Config) {
+	body := "{'text':'Hello, again!'}"
+	bb, err := json.Marshal(body)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.Post(cfg.Slack.WebHook, "application/json", bytes.NewBuffer(bb))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("status: ", resp.Status)
+	//curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
 }
